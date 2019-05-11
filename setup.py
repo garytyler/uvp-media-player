dd77b08a303c03bd89a0e5c4607ab53739836f61
Gary Tyler <mail@garytyler.com>
2019-05-10 23:18:45 -0400

    Include pre-commit dependencies and configuration

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup
 
-
 setup(
     name="eventvr-player",
     version="0.1",
     module=["eventvr_player"],
     include_package_data=True,
     install_requires=["click", "PyQt5", "python-vlc"],
-    # entry_points={"console_scripts": ["eventvrplayer=eventvr_player:main"]},
 )
