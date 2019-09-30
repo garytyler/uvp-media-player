ba1212afb8c692f16c99104b558628ad4376373a
Gary <g@Garys-iMac.local>
2019-09-29 19:02:15 -0700

    Handle missing libs in Mac OS app bundle
    Still numerous issues to work out for Mac OS Build

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import sys
 from os import path
-
+from glob import glob
 import fbs
 from fbs.cmdline import command
 
@@ -26,9 +26,53 @@ def _move_libvlc_dlls_to_subdirectory(frozen_dir):
     for file_path in libvlc_dll_paths:
         shutil.move(file_path, libvlc_dir_path)
 
+def _include_tcl_tk_lib_files_in_bundle():
+    """https://github.com/pyinstaller/pyinstaller/issues/3753"""
 
-@command
-def postfreeze():
+    app_name = fbs.SETTINGS["app_name"]
+    project_dir = fbs.SETTINGS["project_dir"]
+    bundle_root = os.path.join(project_dir, "target", f"{app_name}.app")
+    bundle_src = os.path.join(bundle_root, "Contents", "MacOS")
+    frozen_dir = os.path.join(project_dir, "target", app_name)
+
+    py_ver_major = str(sys.version_info.major)
+    py_ver_minor = str(sys.version_info.minor)
+    py_ver_major_minor  = '.'.join((py_ver_major, py_ver_minor))
+    py_ver_dir = '/Library/Frameworks/Python.Framework/Versions'                    
+    py_lib_dir = os.path.join(py_ver_dir, py_ver_major_minor, 'lib')
+    
+    #src_paths = []
+    #for src_name in os.listdir(py_lib_dir):
+    #    if any((src_name.startswith(s) for s in ('tcl', 'tk', 'Tk'))):
+    #        src_paths.append(os.path.join(py_lib_dir, src_name))
+    
+    for dir_name in ('tcl', 'tk'):
+        targ_dir = os.path.join(bundle_src, dir_name)
+
+        if os.path.isdir(targ_dir):
+            shutil.rmtree(targ_dir)
+        elif os.path.isfile(targ_dir):
+            os.remove(targ_dir)
+
+        os.makedirs(targ_dir)
+
+    for stringstart in ("tcl", "tk", "Tk"):  
+        src_paths = []
+        for src_name in os.listdir(py_lib_dir):
+            if src_name.startswith(stringstart) :
+                src_paths.append(os.path.join(py_lib_dir, src_name))
+        
+        targ_dir = os.path.join(bundle_src, stringstart.lower())
+        for src_path in src_paths:
+            if os.path.isfile(src_path):
+                src_name = os.path.basename(src_path)
+                targ_path = os.path.join(targ_dir, src_name)
+                shutil.copy(src_path, targ_path)
+            elif os.path.isdir(src_path):
+                shutil.copytree(src_path, targ_dir) 
+
+
+def _postfreeze():
     """Make any necessary modifications to the frozen target"""
     log = logging.getLogger()
     app_name = fbs.SETTINGS["app_name"]
@@ -38,6 +82,10 @@ def postfreeze():
     if sys.platform.startswith("win"):
         pass
     elif sys.platform.startswith("darwin"):
+        _include_tcl_tk_lib_files_in_bundle()
+        bundle_root = os.path.join(project_dir, "target", f"{app_name}.app")
+        bundle_src = os.path.join(bundle_root, "Contents", "MacOS")
+        shutil.copytree(os.path.join(frozen_dir, "lib2to3"), os.path.join(bundle_src, "lib2to3"))
         pass
     else:
         _move_libvlc_dlls_to_subdirectory(frozen_dir)
@@ -46,10 +94,16 @@ def postfreeze():
     log.info("Post-freeze complete.")
 
 
+@command
+def postfreeze():
+    fbs.init(project_dir=path.dirname(__file__))
+    _postfreeze()
+
+
 def post_command():
     command = sys.argv[1]
     if command == "freeze":
-        postfreeze()
+        _postfreeze()
 
 
 def main():
