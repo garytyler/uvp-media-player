98d60152835cb4d295545a3cd395df3ff5f53daa
Gary Tyler <mail@garytyler.com>
2019-11-30 17:48:43 -0500

    Use QSettings for managing and storing configuration options

--- src/main/python/util/config.py
+++ src/main/python/util/config.py
@@ -1,6 +1,7 @@
-import json
 import logging
 from os import getenv
+
+from PyQt5.QtCore import QSettings
 
 log = logging.getLogger(__name__)
 
@@ -8,13 +9,17 @@ _CONFIG_FILE_PATH = getenv("VR_PLAYER_CONFIG", ".user_config")
 
 
 _SETTINGS = {
-    "loop_mode": {"options": ("off", "one", "all"), "default": "off"},
-    "stay_on_top": {"options": (True, False), "default": False},
-    "view_scale": {"options": (0.25, 0.5, 1, 2), "default": 1},
-    "color_theme": {"options": ("light", "dark"), "default": "dark"},
-    "url": {"options": (), "default": "wss://seevr.herokuapp.com/mediaplayer"},
-    "volume": {"options": (), "default": 50},  # number between 1 and 100
-    "tool_bar_area": {"options": ("top", "bottom"), "default": "bottom"},
+    "loop_mode": {"options": ("off", "one", "all"), "default": "off", "type": str},
+    "stay_on_top": {"options": (True, False), "default": False, "type": bool},
+    "view_scale": {"options": (0.25, 0.5, 1, 2), "default": 1, "type": int},
+    "color_theme": {"options": ("light", "dark"), "default": "dark", "type": str},
+    "url": {
+        "options": (),
+        "default": "wss://seevr.herokuapp.com/mediaplayer",
+        "type": str,
+    },
+    "volume": {"options": (), "default": 50, "type": int},  # number between 1 and 100
+    "tool_bar_area": {"options": ("top", "bottom"), "default": "bottom", "type": str},
     "meta_tags": {
         "options": ([]),
         "default": [
@@ -31,39 +36,50 @@ _SETTINGS = {
             "disc number",
             "date",
         ],
+        "type": str,
     },
 }
 
 
+class _Settings(QSettings):
+    def __init__(self):
+        super().__init__("SeeVR", "Player")
+        self.setDefaultFormat(self.IniFormat)
+
+
 class _State:
     _handlers: dict = {}
+    settings = _Settings()
 
     def __init__(self):
         # Load config from environment variables
         _state = {}
-        env_url = getenv("VR_PLAYER_REMOTE_URL", default=None)
-        if env_url:
-            _state["url"] = env_url
+        # env_url = getenv("VR_PLAYER_REMOTE_URL", default=None)
+        # if env_url:
+        #     _state["url"] = env_url
 
         # Set values from env vars to local super var
         # Do not call self.__setattr__
         super(_State, self).__setattr__("_state", _state)
 
     def load(self):
-        try:
-            with open(_CONFIG_FILE_PATH, "r") as f:
-                data = json.loads(f.read())
-        except FileNotFoundError:
-            log.info("NO CONFIG FILE FOUND")
-            return
+        for key in _SETTINGS.keys():
+            self.settings.setValue(key, _SETTINGS[key]["default"])
+        # try:
+        #     with open(_CONFIG_FILE_PATH, "r") as f:
+        #         data = json.loads(f.read())
+        # except FileNotFoundError:
+        #     log.info("NO CONFIG FILE FOUND")
+        #     return
 
-        for key, value in data.items():
-            allowed_values = _SETTINGS[key]["options"]
-            if allowed_values and value not in allowed_values:
-                raise ValueError(f"Invalid value found in config: '{key}': {value}")
-            else:
-                self._update_runtime(key, value)
-        log.debug(f"LOADED CONFIG FROM FILE items={self._state}")
+        # for key, value in data.items():
+        #     allowed_values = _SETTINGS[key]["options"]
+        #     if allowed_values and value not in allowed_values:
+        #         raise ValueError(f"Invalid value found in config: '{key}': {value}")
+        #     else:
+        #         self._update_runtime(key, value)
+        #         self.settings.setValue(key, value)
+        # log.debug(f"LOADED CONFIG FROM FILE items={self._state}")
 
     def register_handler(self, key, callback):
         if key not in _SETTINGS.keys():
@@ -81,27 +97,37 @@ class _State:
         self._state[key] = value
 
     def __setattr__(self, key, value):
-        log.debug(f"SET CONFIG ITEM key={key}, value={value}")
-        if self._state.get(key) == value:
-            return
+
+        # log.debug(f"SET CONFIG ITEM key={key}, value={value}")
+        # if self._state.get(key) == value:
+        #     return
 
         allowed_values = _SETTINGS[key]["options"]  # Never throws error
         if allowed_values and value not in allowed_values:
             raise ValueError("Invalid value for this configuration setting")
 
-        self._update_runtime(key, value)
+        self.settings.setValue(key, value)
 
-        with open(_CONFIG_FILE_PATH, "w") as f:
-            f.write(json.dumps(self._state))
+        # self._update_runtime(key, value)
+
+        # with open(_CONFIG_FILE_PATH, "w") as f:
+        #     f.write(json.dumps(self._state))
 
     def __getattr__(self, key):
-        try:
-            value = self._state.get(key, _SETTINGS[key]["default"])
-        except KeyError as e:
-            log.critical(e)
+        if self.settings.contains(key):
+            value = self.settings.value(key)
         else:
-            log.debug(f"GET CONFIG ITEM key={key}, value={value}")
-            return value
+            value = _SETTINGS[key]["default"]
+        return _SETTINGS[key]["type"](value)
+
+        # try:
+        #     value = self.settings.value(key)
+        #     # value = self._state.get(key, _SETTINGS[key]["default"])
+        # except KeyError as e:
+        #     log.critical(e)
+        # else:
+        #     log.debug(f"GET CONFIG ITEM key={key}, value={value}")
+        #     return value
 
 
 state = _State()
