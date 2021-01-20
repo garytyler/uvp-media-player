c4f255be439dd5fd6907cedca1c2dfb1c5f3403d
Gary Tyler <mail@garytyler.com>
2021-01-20 12:16:39 -0500

    👷 Improve CI with testing and test coverage reporting

--- .github/scripts/install-vlc-win.ps1
+++ .github/scripts/install-vlc-win.ps1
@@ -1,6 +1,5 @@
 Set-Variable -Name VLC_VERSION -Value "3.0.11"
 Set-Variable -Name VLC_INSTALLER -Value "~\Downloads\vlc-${VLC_VERSION}-win64.exe"
-Set-Variable -Name VLC_ROOT -Value "~\Downloads\vlc-${VLC_VERSION}"
 Set-Variable -Name VLC_DL_URL -Value "https://download.videolan.org/pub/vlc/${VLC_VERSION}/win64/vlc-${VLC_VERSION}-win64.exe"
 Write-Host "Downloading VLC installer..."
 Invoke-WebRequest -Method Get -OutFile "$VLC_INSTALLER" -Uri "$VLC_DL_URL"
